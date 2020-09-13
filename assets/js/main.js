130acc1e320d6afe53e5f53030775e0ea35d932b
maeva merrou <maeva.merrou@live.fr>
2020-09-14 00:57:31 +0200

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,7 @@
 	// Breakpoints.
 		breakpoints({
 			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
+			large:    [ '981px',   '1024px' ],
 			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
 			xsmall:   [ null,      '480px'  ]
